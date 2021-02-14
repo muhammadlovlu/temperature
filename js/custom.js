83004a7eba04a1b6f6db1c1a20d351165df91a9f
muhammadlovlu <chyshahab@gmail.com>
2021-02-15 01:35:50 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,7 @@
 
 
 document.getElementById("button").addEventListener('click', function(){
-   
     const inputValue = document.getElementById('input-value').value;
-    
     const api = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=a4d2ed67337e5615eca12faa6a95c54f`;
     fetch(api)
     .then (res => res.json())
