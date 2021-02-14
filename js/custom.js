59b8f3eeaa446500c5b51af985d2bdb190368071
muhammadlovlu <chyshahab@gmail.com>
2021-02-15 01:56:55 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -9,7 +9,6 @@
 document.getElementById("search").addEventListener('click', function(){
     const inputValue = document.getElementById('input-value').value;
     const api = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=a4d2ed67337e5615eca12faa6a95c54f`;
-    console.log(api)
     fetch(api)
     .then (res => res.json())
     .then(data => {    
@@ -20,6 +19,7 @@ document.getElementById("search").addEventListener('click', function(){
         const status = document.getElementById('update-status');
         status.innerText = data.weather[0].description;
     })
+    .catch(error => alert("Type a valid city Name !"))
 })
            
            
