45bd88380f80e4d3a545e217ca322996700187c2
muhammadlovlu <chyshahab@gmail.com>
2021-02-15 01:32:44 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-//             a4d2ed67337e5615eca12faa6a95c54f
+           // a4d2ed67337e5615eca12faa6a95c54f
 // fetch("https://api.openweathermap.org/data/2.5/weather?q=london&appid=a4d2ed67337e5615eca12faa6a95c54f")
 // .then (res => res.json())
 // .then(data => {
@@ -7,14 +7,23 @@
 
 
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=london&appid=a4d2ed67337e5615eca12faa6a95c54f")
-.then (res => res.json())
-.then(data => {
-    const location = document.getElementById("location");
-    location.innerText = data.name;
-    const temperature = document.getElementById('temperature');
-    temperature.innerText = data.main.temp;
-    const status = document.getElementById('update-status');
-    status.innerText = data.weather[0].description;
-})
 
+document.getElementById("button").addEventListener('click', function(){
+   
+    const inputValue = document.getElementById('input-value').value;
+    
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=a4d2ed67337e5615eca12faa6a95c54f`;
+    fetch(api)
+    .then (res => res.json())
+    .then(data => {    
+        const location = document.getElementById("location");
+        location.innerText = data.name;
+        const temperature = document.getElementById('temperature');
+        temperature.innerText = data.main.temp;
+        const status = document.getElementById('update-status');
+        status.innerText = data.weather[0].description;
+    })
+})
+           
+           
+ 
