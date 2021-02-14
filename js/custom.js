1193d7774b40aabea61b6488c2b080f34b913103
muhammadlovlu <chyshahab@gmail.com>
2021-02-14 19:52:46 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=london&appid=a4d2ed67337e5615eca12faa6a95c54f')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=a4d2ed67337e5615eca12faa6a95c54f')
 .then (response => response.json())
 .then (data => {
     console.log(data)
 })
 
+function searchReport(){
+
+}
