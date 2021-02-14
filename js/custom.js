fa9927a6bbfa9654692214bcefccfde6ab77d272
muhammadlovlu <chyshahab@gmail.com>
2021-02-14 22:47:04 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,20 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=a4d2ed67337e5615eca12faa6a95c54f')
-.then (response => response.json())
-.then (data => {
-    console.log(data)
+//             a4d2ed67337e5615eca12faa6a95c54f
+// fetch("https://api.openweathermap.org/data/2.5/weather?q=london&appid=a4d2ed67337e5615eca12faa6a95c54f")
+// .then (res => res.json())
+// .then(data => {
+//     console.log(data)
+// })
+
+
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=london&appid=a4d2ed67337e5615eca12faa6a95c54f")
+.then (res => res.json())
+.then(data => {
+    const location = document.getElementById("location");
+    location.innerText = data.name;
+    const temperature = document.getElementById('temperature');
+    temperature.innerText = data.main.temp;
+    const status = document.getElementById('update-status');
+    status.innerText = data.weather[0].description;
 })
 
-function searchReport(){
-
-}
