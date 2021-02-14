5c516e50bb129c58d6a2f6b4a556544af120b232
muhammadlovlu <chyshahab@gmail.com>
2021-02-15 01:48:08 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
-           // a4d2ed67337e5615eca12faa6a95c54f
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=london&appid=a4d2ed67337e5615eca12faa6a95c54f")
+//          //  a4d2ed67337e5615eca12faa6a95c54f
+// fetch("https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=a4d2ed67337e5615eca12faa6a95c54f")
 // .then (res => res.json())
 // .then(data => {
 //     console.log(data)
@@ -8,7 +8,7 @@
 
 document.getElementById("search").addEventListener('click', function(){
     const inputValue = document.getElementById('input-value').value;
-    const api = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=a4d2ed67337e5615eca12faa6a95c54f`;
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=a4d2ed67337e5615eca12faa6a95c54f`;
     console.log(api)
     fetch(api)
     .then (res => res.json())
