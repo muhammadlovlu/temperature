4a9a5380f4b2ddf5f08a36aff983be8abf25d910
muhammadlovlu <chyshahab@gmail.com>
2021-02-14 16:04:01 +0600

    updated

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+fetch('https://api.openweathermap.org/data/2.5/weather?q=london&appid=a4d2ed67337e5615eca12faa6a95c54f')
+.then (response => response.json())
+.then (data => {
+    console.log(data)
+})
+
