383850e3e9e3082b713180bb1235eafe4ee8d15e
muhammadlovlu <chyshahab@gmail.com>
2021-02-15 02:30:15 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,9 @@
-//          //  a4d2ed67337e5615eca12faa6a95c54f
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=a4d2ed67337e5615eca12faa6a95c54f")
-// .then (res => res.json())
-// .then(data => {
-//     console.log(data)
-// })
-
-
 document.getElementById("search").addEventListener('click', function(){
     const inputValue = document.getElementById('input-value').value;
     const api = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=a4d2ed67337e5615eca12faa6a95c54f`;
     fetch(api)
     .then (res => res.json())
-    .then(data => {    
+    .then(data => {  
         const location = document.getElementById("location");
         location.innerText = data.name;
         const temperature = document.getElementById('temperature');
@@ -22,5 +14,3 @@ document.getElementById("search").addEventListener('click', function(){
     .catch(error => alert("Type a valid city Name !"))
 })
            
-           
- 
